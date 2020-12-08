d6e6eb5f0f677f5a1f718bc993b310470825c18b
JAFB-201801287 <75402151+JAFB-201801287@users.noreply.github.com>
2020-12-08 12:45:45 -0600

    Creacion de modelo

---
+++ model.py/candidato.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+class Candidato():
+
+    def __init__(self):
+        self.id = 0
+        self.nombre = ""
+        self.apellido = ""
+        self.edad = 0
+        self.puesto = ""
+        self.salario = 0.0
+
+# SETTERS
+
+    def setId(self, id):
+        self.id = id
+
+    def setNombre(self, nombre):
+        self.nombre = nombre
+
+    def setApellido(self, apellido):
+        self.apellido = apellido
+
+    def setEdad(self, edad):
+        self.edad = edad
+
+    def setPuesto(self, puesto):
+        self.puesto = puesto
+
+    def setSalario(self, salario):
+        self.salario = salario
+
+# GETTERS
+
+    def getId(self):
+        return self.id
+
+    def getNombre(self):
+        return self.nombre
+
+    def getApellido(self):
+        return self.apellido
+
+    def getEdad(self):
+        return self.edad
+    
+    def getPuesto(self):
+        return self.puesto
+    
+    def getSalario(self):
+        return self.salario
