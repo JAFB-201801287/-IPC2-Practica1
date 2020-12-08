f164388c65c6a25e947aaed891fefbf1307031f8
JAFB-201801287 <75402151+JAFB-201801287@users.noreply.github.com>
2020-12-08 12:33:41 -0600

    Funcion para menu de la aplicacion

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+import menu as Menu
+
+Menu.menu()
--- menu.py
+++ menu.py
@@ -4,3 +4,38 @@ import os
 
 def clean():  # Metodo para limpiar pantalla
     os.system("cls")
+
+def caratula():
+    clean()
+    print("==".center(150, "="))
+    print("\tIPC2")
+    print("\tJose Andres Flores Barco")
+    print("\t201801287")
+    print("==".title().center(150, "="))
+
+def menu():
+    opcionMenu = 0
+    caratula()
+    while True:
+        print(" Menu ".title().center(150, "="))
+        print("\t1) - Lectura de archivo CSV:")
+        print("\t2) - Cálculo de Datos:")
+        print("\t3) - Generación de archivo JSON.:")
+        print("\t4) - Salir:".title())
+        print("")
+
+        # solicituamos una opción al usuario
+        opcionMenu = input("inserta un numero valor >> ")
+        print("".center(150, "_"))
+        if opcionMenu == "1":
+            continue
+        elif opcionMenu == "2":
+            continue
+        elif opcionMenu == "3":
+            continue
+        elif opcionMenu == "4":
+            break
+        else:
+            print("")
+            input(
+                "No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
